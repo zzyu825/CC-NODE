6276e63f754241b86adbfe125f020e14342a5e10
zhengzhiyu <zhengzhiyu825@163.com>
2020-06-13 19:01:33 +0800

    1-02

---
+++ index.js
@@ -0,0 +1,23 @@
+// console.log(global);
+
+// const timer = setTimeout(() => {}, 1000);
+// const timer = setImmediate(() => {}, 0);
+// console.log(timer);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// const buffer = Buffer.from('abcdefg', 'utf-8');
+// console.log(buffer);
+
+// console.log("当前命令行：", process.cwd());
+
+// setImmediate(() => {
+//     console.log(1)
+// }, 0);
+// process.exit();
+
+// console.log(process.argv);
+// console.log(process.platform);
+console.log(process.kill(11408));
+// console.log(process.env);
---
+++ src/index.js
@@ -0,0 +1,2 @@
+// console.log(__dirname);
+console.log("当前命令行：", process.cwd());
