82d97348b38ef042b51bd045a63e1c0f447e8145
zhengzhiyu <zhengzhiyu825@163.com>
2020-09-20 10:07:33 +0800

    3-09

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-require("./models/relation");
+const moment = require("moment");
+moment.locale("zh-cn");
 
-const md5 = require('md5');
-// console.log(md5(''));
-// console.log(md5('asdzxcasdzxcasdzxcasdzxcasdzxcasdzxc'));
+// 得到当前时间，moment对象
+console.log(moment().toString()); // 本地
+console.log(moment.utc().toString()); // utc
 
-const adminServ = require('./services/adminService');
-// adminServ.addAdmin({
-//     loginId: 'zzy',
-//     loginPwd: 123
-// });
-// adminServ.addAdmin({
-//     loginId: 'zzyu',
-//     loginPwd: 'asdZZY.95'
-// });
-// adminServ.updateAdmin(5, {
-//     loginPwd: '123'
-// });
-adminServ.login("zzyu", "123").then((r) => {
-    console.log(r);
-});
+// 得到当前时间戳
+console.log(moment().valueOf(), +moment());
+console.log(moment.utc().valueOf(), +moment.utc());
 
+// 根据指定的时间格式(xxxx-xx-xx)得到时间、时间戳
+// console.log(moment(0).toString(), +moment(0));
+// console.log(moment.utc(0).toString(), +moment.utc(0));
+const value = "1970-01-01 00:00:00";
+console.log(moment(value).toString(), +moment(value));
+console.log(moment.utc(value).toString(), +moment.utc(value));
+
+// 使用日期令牌转换
+// 令牌是一个格式化的字符串
+const formats = ["YYYY-MM-DD HH:mm:ss", "YYYY-M-D H:m:s", "x"];
+console.log(moment.utc("1970-01-01 00:00:00", formats, true).toString());
+console.log(moment.utc("1970-1-1 0:0:0", formats, true).toString());
+console.log(moment.utc(0, formats, true).toString());
+const invalidMoment = moment.utc(
+  "Thu Jan 01 1970 00:00:00 GMT+0000",
+  formats,
+  true
+);
+console.log(invalidMoment.isValid()); // false
+console.log(invalidMoment.toString());
+console.log(+invalidMoment);
+
+// 显示（发生在客户端）
+// const m = moment.utc("2015-1-5 23:00:01", formats, true);
+// console.log(m.local().format("YYYY年MM月DD日 HH点mm分ss秒"));
+
+// const m = moment("2015-1-5 23:00:01", formats, true);
+// const toServer = m.utc().format("YYYY-MM-DD HH:mm:ss");
+// console.log(toServer);
+
+const m = moment.utc("2020-9-20 2:00:01", formats, true);
+console.log(m.local().fromNow());
