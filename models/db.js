932b745cfd825ce0afb461a1f614d754e34eee08
zhengzhiyu <zhengzhiyu825@163.com>
2020-09-25 19:04:05 +0800

    3-12

--- models/db.js
+++ models/db.js
@@ -1,9 +1,12 @@
 const { Sequelize, DataTypes } = require('sequelize');
+const { sqlLogger } = require("../logger");
 
 const sequelize = new Sequelize('myschooldb', 'root', '950825', {
   host: 'localhost',
   dialect: 'mysql',
-  logging: null
+  logging: msg => {
+    sqlLogger.debug(msg);
+  }
 });
 
 module.exports = {
