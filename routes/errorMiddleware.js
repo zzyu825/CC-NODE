b62eeb03914b418c6df4295d2e35ac9179485e95
zhengzhiyu <zhengzhiyu825@163.com>
2020-09-28 19:39:44 +0800

    4-05

--- routes/errorMiddleware.js
+++ routes/errorMiddleware.js
@@ -1,13 +1,10 @@
 // 处理错误的中间件
-
+const send = require("./getSendResult");
 module.exports = (err, req, res, next) => {
   if (err) {
-    const errObj = {
-      code: 500,
-      msg: err instanceof Error ? err.message : err
-    };
+    const errObj = err instanceof Error ? err.message : err;
     // 发生了错误
-    res.status(500).send(errObj);
+    res.status(500).send(send.getErr(errObj));
   } else {
     next();
   }
--- routes/init.js
+++ routes/init.js
@@ -1,27 +1,21 @@
 const express = require("express");
 const app = express();
+
+// 映射public目录中的静态资源
 const path = require("path");
 const staticRoot = path.resolve(__dirname, "../public");
+app.use(express.static(staticRoot));
 
-/**
- * 下面代码的作用：
- * 当请求时，会根据请求路径（req.path）从指定的目录中寻找是否存在该文件，如果存在，直接响应文件内容，而不再移交给后续的中间件
- * 如果不存在该文件，则直接移交给后续的中间件处理
- * 默认情况下，如果映射的结果是一个目录，则会自动使用index.html
- */
-// app.use(express.static(path.resolve(__dirname, "../public")));
-// app.use(express.static(staticRoot, {
-//   index: "default.html" // 默认为index.html
-// }));
+// 解析 application/x-www-form-urlencoded 格式的请求体
+app.use(express.urlencoded({ extend: true }));
 
-// app.use(express.urlencoded({ extend: true }));
-app.use(require("./myUrlEncoded")); // 所有请求都会先经过自定义中间件处理
+// 解析 application/json 格式的请求体
 app.use(express.json());
 
-app.post("/api/student", (req, res) => {
-  console.log(req.body);
-});
+// 处理api的请求
+app.use("/api/student", require("./api/student"));
 
+// 处理错误的中间件
 app.use(require("./errorMiddleware"));
 
 const port = 12306;
