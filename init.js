c97e364ed6027f92d43c9961a13d9b6133e44c35
zhengzhiyu <zhengzhiyu825@163.com>
2020-10-13 11:28:06 +0800

    5-01

--- init.js
+++
@@ -1,3 +0,0 @@
-require('./models/init');
-require('./services/init');
-require('./routes/init')
--- routes/proxyMid.js
+++
@@ -1,9 +0,0 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-const context = "/data";
-module.exports = createProxyMiddleware(context, {
-  target: "http://yuanjin.tech:5100",
-  pathRewrite: function (path, req) {
-    return path.substr(context.length);
-  }
-})
